b1adeaba5bf757610dd8bb4a0fb7f4fb1f7e274b
ex02153 <willem.denys@digipolis.be>
2015-06-11 12:40:58 +0200

    Release v1.0.2

--- dist/awelzijn-notification-callout.js
+++ dist/awelzijn-notification-callout.js
@@ -8,8 +8,17 @@
   module.directive('aWelzijnNotificationCallout', [function () {
     return {
       restrict: 'E',
-      templateUrl: 'template.html'
+      templateUrl: 'templates/template.html'
     };
   }]);
 })();
-;
+;angular.module('tink.skeleton').run(['$templateCache', function($templateCache) {
+  'use strict';
+
+  $templateCache.put('templates/template.html',
+    "<ul class=errorMessages> <li ng-repeat=\"error in errors\"> <div class=\"alert callout-danger\" role=alert> <strong ng-show=error.title>{{error.title}}</strong>{{error.message}} </div> </li> </ul> <ul class=successMessages> <li ng-repeat=\"msg in messages\"> <div class=\"alert callout-success alert-dismissible\" role=alert> <a class=\"close float-right\" data-dismiss=alert aria-label=Sluit><i class=\"fa fa-times\"></i></a>\n" +
+    "<strong ng-show=msg.title>{{msg.title}}</strong>{{msg.message}} </div> </li> </ul> <ul class=warningMessages> <li ng-repeat=\"warning in warnings\"> <div class=\"alert callout-warning alert-dismissible\" role=alert> <a class=\"close float-right\" data-dismiss=alert aria-label=Sluit><i class=\"fa fa-times\"></i></a>\n" +
+    "<strong ng-show=warning.title>{{warning.title}}</strong>{{warning.message}} </div> </li> </ul>"
+  );
+
+}]);
